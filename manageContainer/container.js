ec7d7dafd01618440d62dc7aae440820d7b25765
Jenyfael <35492428+Jenyfael@users.noreply.github.com>
2018-06-25 17:54:07 +0200

    Pause/Unpause and restart

--- manageContainer/container.js
+++ manageContainer/container.js
@@ -8,12 +8,9 @@ let errorLogger = require('../error.js');
 //@param
 //dockerType : type d'image voulu (ex : nodejs, mongodb)
 //name : nom à attribuer à l'image
-function createDockerImage(containerType)
-{
-    return new Promise(function (resolve, reject)
-    {
-        if (containerType === "" || containerType === undefined)
-        {
+function createDockerImage(containerType) {
+    return new Promise(function (resolve, reject) {
+        if (containerType === "" || containerType === undefined) {
             console.log("their is no dockerType field");
             errorLogger.logError("unable to create docker image : their is no dockerType field");
             reject({err: 1, msg: "no DockerType found"});
@@ -23,19 +20,16 @@ function createDockerImage(containerType)
         while (catalog.length > i && containerType != catalog[i].type) {
             i++
         }
-        if (i === catalog.length)
-        {
+        if (i === catalog.length) {
             reject({err: 1, msg: "DockerType not existing into catalog"});
         }
         exec("docker build /Users/mathisfasolo/WebstormProjects/Freighter/manageContainer/template/" + containerType + " -t " + containerType, (err, stdout, stderr) => {
-            if (err)
-            {
+            if (err) {
                 console.log(err);
                 errorLogger.logError("unable to create docker image : " + err);
                 reject({err: 2, msg: "Failed to initialized docker"});
             }
-            else
-            {
+            else {
                 console.log(stdout);
                 resolve({err: 0, msg: "Docker Image succefully created"});
             }
@@ -49,12 +43,9 @@ function createDockerImage(containerType)
 //@param
 //dockerType : type d'image voulu (ex : nodejs, mongodb)
 //name : nom du volume (même nom que l'image correspondante)
-function createVolume (containerType, containerName)
-{
-    return new Promise(function (resolve, reject)
-    {
-        if (containerType === "" || containerType === undefined)
-        {
+function createVolume(containerType, containerName) {
+    return new Promise(function (resolve, reject) {
+        if (containerType === "" || containerType === undefined) {
             console.log("their is no dockerType field");
             errorLogger.logError("unable to create docker volume : their is no dockerType field");
             reject({err: 1, msg: "no DockerType found"});
@@ -90,10 +81,8 @@ function createVolume (containerType, containerName)
 //@param
 //dockerType : type d'image voulu (ex : nodejs, mongodb)
 //name : nom de l'image
-function startContainer(containerType, containerName, cpu, ram)
-{
-    return new Promise(function (resolve, reject)
-    {
+function startContainer(containerType, containerName, cpu, ram) {
+    return new Promise(function (resolve, reject) {
         let catalog = catalogue.getCatalog();
         let config = getSystemConfig();
         let cpuLimitation;
@@ -133,8 +122,8 @@ function startContainer(containerType, containerName, cpu, ram)
         }
         let p = 0;
         console.log('test');
-        console.log("portlist length "+config.portList[p]);
-        while (arrayPortLength > p && config.portList[p]===true) {
+        console.log("portlist length " + config.portList[p]);
+        while (arrayPortLength > p && config.portList[p] === true) {
             p++;
         }
         if (p === arrayPortLength) {
@@ -142,13 +131,13 @@ function startContainer(containerType, containerName, cpu, ram)
         } else {
             port = 46000 + p;
         }
-        exec("docker run --name " + containerName + " -m " + ramLimitation + " --cpus=" + cpuLimitation + " -p " + port + ":8080 --expose=8080 --mount source=" + containerName+ ",target=/home  " + containerType, (err, stdout, stderr) => {
+        exec("docker run --name " + containerName + " -m " + ramLimitation + " --cpus=" + cpuLimitation + " -p " + port + ":8080 --expose=8080 --mount source=" + containerName + ",target=/home  " + containerType, (err, stdout, stderr) => {
             if (err) {
                 console.log(err);
                 reject({err: 2, msg: "Failed to start container"});
             } else {
                 let docker = {
-                    "id" : util.generateRandomID(),
+                    "id": util.generateRandomID(),
                     "nameApp": containerName,
                     "type": containerType,
                     "RAM_allowed": ramLimitation,
@@ -176,100 +165,106 @@ function startContainer(containerType, containerName, cpu, ram)
  * @param ram (optionnel)
  * @returns {Promise<*>}
  */
-module.exports.deployContainer = async function (typeContainer, nameContainer, cpu, ram)
-{
+module.exports.deployContainer = async function (typeContainer, nameContainer, cpu, ram) {
     createDockerImage(typeContainer)
-        .then(function (res)
-        {
+        .then(function (res) {
             console.log(res);
             createVolume(typeContainer, nameContainer)
-                .then(function (res)
-                {
+                .then(function (res) {
                     startContainer(typeContainer, nameContainer, cpu, ram)
-                        .then(function ()
-                        {
+                        .then(function () {
                             return {"err": "0", "msg": "Container succefully created and started"}
                         })
-                        .catch(function (err)
-                        {
+                        .catch(function (err) {
                             return err;
                         });
                 })
-                .catch(function (err)
-                {
+                .catch(function (err) {
                     return err;
                 });
         })
-        .catch(function (err)
-        {
+        .catch(function (err) {
             return err;
         });
+};
+
+module.exports.restart=function(nameContainer){
+    this.pauseContainer(nameContainer).then(function(res){
+        console.log(res);
+        this.unpauseContainer(nameContainer).then(function(res){
+            return {err: 0, msg: "Container succefully created and started"};
+        })
+    })
 };
 
 //met un docker existant en pause
 //@param
 //name : nom du container à mettre en pause
 module.exports.pauseContainer = async function (nameContainer) {
-    let config = getSystemConfig();
-    if (nameContainer === "" || typeof nameContainer === undefined) {
-        errorLogger.logError("their is no name field");
-        return {"err": "1", "msg": "no name found"}
-    }
-    let i = 0;
-    while (i < config.containersList.length && config.containersList[i].id !== nameContainer) {
-        i++;
-    }
-    if (i === config.containersList.length) {
-        errorLogger.logError("unable to pause container : " + nameContainer + " this container does not exist");
-        return {"err": "15", "msg": "This container does not exist"}
-    }
-    if (config.containersList[i].pause === true) {
-        errorLogger.logError("unable to pause container : " + nameContainer + " container already in pause states");
-        return {"err": "11", "msg": "Container already in pause states"}
-    }
-    exec("docker pause " + nameContainer, (err, stdout, stderr) => {
-        if (err) {
-            console.log("Error occur during container pause");
-            errorLogger.logError("unable to pause container : " + nameContainer + " error occur during container pause");
-            return {"err": "2", "msg": "Failed to pause container"}
-        } else {
-            config.containersList[i].isStarted = false;
-            putConfig(config);
-            return {"err": "0", "msg": "Container succefully passed in paused"}
+    return new Promise(function (resolve, reject) {
+        let config = getSystemConfig();
+        if (nameContainer === "" || nameContainer === undefined) {
+            errorLogger.logError("their is no name field");
+            reject({err: 1, msg: "no name found"});
         }
+        let i = 0;
+        while (i < config.containersList.length && config.containersList[i].id !== nameContainer) {
+            i++;
+        }
+        if (i === config.containersList.length) {
+            errorLogger.logError("unable to pause container : " + nameContainer + " this container does not exist");
+            reject({err: 15, msg: "This container does not exist"});
+        }
+        if (config.containersList[i].pause === true) {
+            errorLogger.logError("unable to pause container : " + nameContainer + " container already in pause states");
+            reject({err: 11, msg: "Container already in pause states"});
+        }
+        exec("docker pause " + nameContainer, (err, stdout, stderr) => {
+            if (err) {
+                console.log("Error occur during container pause");
+                errorLogger.logError("unable to pause container : " + nameContainer + " error occur during container pause");
+                reject({err: 2, msg: "Failed to pause container"});
+            } else {
+                config.containersList[i].isStarted = false;
+                putConfig(config);
+                resolve({err: 0, msg: "Container succefully passed in paused"});
+            }
+        })
     })
 };
 
 //relance un container mis en pause
 //@param
 //name : nom du container
-async function unpauseContainer(req) {
-    if (req.name === "" || typeof req.name === undefined) {
-        console.log("their is no name field");
-        return {"err": "1", "msg": "no name found"}
-    }
-    let config = await getConfig();
-    let i = 0;
-    while (i < config.containersList.length && config.containersList[i]._id !== req.name) {
-        i++;
-    }
-    if (i === config.containersList.length) {
-        return {"err": "15", "msg": "This container does not exist"}
-    }
-    if (config.containersList[i].pause === false) {
-        return {"err": "11", "msg": "Container already in running states"}
-    }
-    exec("docker pause " + req.name, (err, stdout, stderr) => {
-        if (err) {
-            console.log("Error occur during docker pause");
-            return {"err": "2", "msg": "Failed to pause container"}
-        } else {
-            config.containersList[i].pause = false;
-            putConfig(config);
-            return {"err": "0", "msg": "Container succefully passed in paused"}
+module.exports.unpauseContainer = function (nameContainer) {
+    return new Promise(function (resolve, reject) {
+        if (nameContainer === "" || nameContainer === undefined) {
+            console.log("their is no name field");
+            reject ({err: 1, msg: "no name found"});
         }
+        let config = getSystemConfig();
+        let i = 0;
+        while (i < config.containersList.length && config.containersList[i].id !== nameContainer) {
+            i++;
+        }
+        if (i === config.containersList.length) {
+            reject({err: 15, msg: "This container does not exist"});
+        }
+        if (config.containersList[i].pause === false) {
+            reject ({err: 11, msg: "Container already in running states"});
+        }
+        exec("docker unpause " + nameContainer, (err, stdout, stderr) => {
+            if (err) {
+                console.log("Error occur during docker pause");
+                reject({err: 2, msg: "Failed to pause container"});
+            } else {
+                config.containersList[i].pause = false;
+                putConfig(config);
+                resolve({err: 0, msg: "Container succefully passed in paused"});
+            }
+        })
     })
-}
+};
 
 //kill les process d'un container
 //@param
@@ -376,9 +371,7 @@ async function deleteContainer(req) {
 }
 
 
-
-function getSystemConfig ()
-{
+function getSystemConfig() {
     return JSON.parse(fs.readFileSync("./system/containerConfig.json", 'utf8'));
 }
 
@@ -395,8 +388,7 @@ function putConfig(config) {
     });
 }
 
-module.exports.getContainerInstalled = function ()
-{
+module.exports.getContainerInstalled = function () {
     const pathToContainerList = "../system/containerConfig.json";
     let json = require(pathToContainerList);
     return json.containersList;
--- routes/index.js
+++ routes/index.js
@@ -119,22 +119,22 @@ router
         containerManagement;
         //res.render('catalogue', {"chooseAContainer" : true, "listContainers" : arrayCatalog});
     })
-    .get('/container/stopContainer', function (req, res, next)
+    .get('/container/stopContainer/:name', function (req, res, next)
     {
         console.log("container stopped !");
-        // TODO  appel fonction container
+        containerManagement.pauseContainer(req.params.name);
         res.send(true);
     })
-    .get('/container/startContainer', function (req, res, next)
+    .get('/container/startContainer/:name', function (req, res, next)
     {
         console.log("container started !");
-        // TODO appel fonction container
+        containerManagement.unpauseContainer(req.params.name);
         res.send(true);
     })
-    .get('/container/restartContainer', function (req, res, next)
+    .get('/container/restartContainer/:name', function (req, res, next)
     {
         console.log("container restarted !");
-        // TODO appel fonction container
+        containerManagement.restart(req.params.name);
         res.send(true);
     });
 
