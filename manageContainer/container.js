1b4b444e2947b4b0e70b1775ec1147f8cc39ffef
mathisFasolo <mathis.fasolo@icloud.com>
2018-06-26 10:04:08 +0200

    Correction du modèle de grille et supression de materialize css ajout du catalogue

--- manageContainer/container.js
+++ manageContainer/container.js
@@ -178,32 +178,34 @@ function startContainer(containerType, containerName, cpu, ram)
  */
 module.exports.deployContainer = async function (typeContainer, nameContainer, cpu, ram)
 {
-    createDockerImage(typeContainer)
-        .then(function (res)
-        {
-            console.log(res);
-            createVolume(typeContainer, nameContainer)
-                .then(function (res)
-                {
-                    startContainer(typeContainer, nameContainer, cpu, ram)
-                        .then(function ()
-                        {
-                            return {"err": "0", "msg": "Container succefully created and started"}
-                        })
-                        .catch(function (err)
-                        {
-                            return err;
-                        });
-                })
-                .catch(function (err)
-                {
-                    return err;
-                });
-        })
-        .catch(function (err)
-        {
-            return err;
-        });
+    return new Promise(function (resolve, reject)
+    {
+        createDockerImage(typeContainer)
+            .then(function (res)
+            {
+                createVolume(typeContainer, nameContainer)
+                    .then(function (res)
+                    {
+                        startContainer(typeContainer, nameContainer, cpu, ram)
+                            .then(function ()
+                            {
+                                resolve({"err": "0", "msg": "Container succefully created and started"});
+                            })
+                            .catch(function (err)
+                            {
+                                return err;
+                            });
+                    })
+                    .catch(function (err)
+                    {
+                        return err;
+                    });
+            })
+            .catch(function (err)
+            {
+                return err;
+            });
+    });
 };
 
 //met un docker existant en pause
